3f0ba5c974fb104c4f9198b053cf10fe9eac16e9
encircleacity <bojie.sun@hotmail.com>
2016-04-12 23:10:26 +0800

    set-up of the one-page personal website

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+/**
+ * Customized Javascript code for Bojie's personal website.
+ */
+
+// Tabs
+$('#work_security a[href="#work_security"]').tab('show');
+$('#work_embedded a[href="#work_embedded"]').tab('show');
+$('#work_web a[href="#work_web"]').tab('show');
+
+
+// Timeline
+
