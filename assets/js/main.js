f3908f2bf482a2fc68142a77ddeb94d533029b77
encircleacity <bojie.sun@hotmail.com>
2016-04-19 00:33:02 +0800

    update page format

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,16 +2,7 @@
  * Customized Javascript code for Bojie's personal website.
  */
 
-// Tabs
-//$('#work_security a[href="#work_security"]').tab('show');
-//$('#work_embedded a[href="#work_embedded"]').tab('show');
-//$('#work_web a[href="#work_web"]').tab('show');
 
-// Tabs enhanced collapse effects
+// Tabs enhanced collapse effects from bootstrap-tabcollapse.js
 $('.content-tabs').tabCollapse();
 
-// initialize tab show function
-$('.nav-tabs a').click(function(e) {
-    e.preventDefault();
-    $(this).tab('show');
-});
