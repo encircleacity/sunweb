39d3e6469f19b82c2605a1a06490909a60416033
encircleacity <bojie.sun@hotmail.com>
2016-04-23 12:58:18 +0800

    page layout and css optimization

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,10 @@
  * Customized Javascript code for Bojie's personal website.
  */
 
+// Navar collapse and expand toggle
+$('.navbar-nav a').on('click', function(){
+   $('.navbar-toggle').click() //bootstrap 3.x
+});
 
 // Tabs enhanced collapse effects from bootstrap-tabcollapse.js
 $('.content-tabs').tabCollapse();
